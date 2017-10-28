4e56002ca0496202501c9a3d9b1b24f6661f6ab1
ketavbhatt <ketavbhatt@gmail.com>
2017-10-28 23:32:49 +0530

    settings

--- MarketWatch/settings.py
+++ MarketWatch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+fom$8vigc1bqlzaozo*)#r5^!arfzh3nb9u^a29o-+#=-gtxz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
